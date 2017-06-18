da27647aeb814bcc1c960bd9c1580207af99ec9f
Rogerio I <roger88ist@gmail.com>
2017-06-18 05:10:36 -0400

    develop gains td

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	populateFields()
+	populateFields();
+	populateProfits();
 });
 
 
@@ -15,6 +16,16 @@ function populateFields() {
 	insertValues(btc);
 }
 
+function populateProfits() {
+	var ltc = litecoinData();
+	var eth = ethereumData();
+	var btc = bitcoinData();	
+
+	insertProfit(ltc);
+	insertProfit(eth);
+	insertProfit(btc);
+}
+
 function insertValues(coinObject) {
 	var table = 'table#' + coinObject.name;
 	var coinsLocation = $(table + ' td')[0];
@@ -22,4 +33,43 @@ function insertValues(coinObject) {
 
 	$(coinsLocation).text(coinObject.coins);
 	$(priceLocation).text(coinObject.pricePerCoin);
+}
+
+function insertProfit(coinObject) {
+	var table = 'table#' + coinObject.name;
+	var moneyLocation = $(table + ' div.split')[0];
+	var percentLocation = $(table + ' div.split')[1];
+
+
+	var change = calculateProfits(coinObject);
+
+
+	$(moneyLocation).text(change.money);
+	$(percentLocation).text(change.percent);
+}
+
+function calculateProfits(coinObject) {
+	var currentPrice = currentPrices()[coinObject.name];
+	var purchasedPrice = coinObject.pricePerCoin;
+
+	return {
+		money: (currentPrice - purchasedPrice).toFixed(2),
+		percent: calculatePercent(currentPrice, purchasedPrice).toFixed(1)
+	}
+}
+
+function calculatePercent(cp, pp) {
+	if (cp == pp) {
+		return 0
+	} else {
+		return ((cp / pp) - 1) * 100;
+	}
+}
+
+function currentPrices() {
+	return {
+		ltc: 10,
+		eth: 300,
+		btc: 2500
+	}
 }
